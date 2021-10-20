8da1500b998a9a3ebb9ed856308b91147426497a
b3nj4hb <benjalive12@gmail.com>
2021-10-19 20:21:06 -0500

    css navbar

--- src/app/admin/presentation/components/navbar/navbar.component.ts
+++ src/app/admin/presentation/components/navbar/navbar.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'adra-navbar',
   templateUrl: './navbar.component.html',
-  styles: [
+  styleUrls: [
+    './navbar.component.css'
   ]
 })
 export class NavbarComponent implements OnInit {
