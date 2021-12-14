5334f9ae9c90393794629f31d38cabad7ad088d8
Digi85 <chrisdigi85@gmail.com>
2021-12-14 01:03:27 -0500

    digi cambios

--- src/app/user/presentation/views/seminarios/seminarios.component.ts
+++ src/app/user/presentation/views/seminarios/seminarios.component.ts
@@ -2,6 +2,7 @@ import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 import { SeminariosService } from './seminarios.service';
 import { Component, OnInit } from '@angular/core';
 import { Seminario } from './seminarios';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'adra-seminarios',
   templateUrl: './seminarios.component.html',
@@ -33,5 +34,17 @@ export class SeminariosComponent implements OnInit {
       this.urlSafe=this.filas[0].url?this.sanitizer.bypassSecurityTrustResourceUrl(this.filas[0].url):this.sanitizer.bypassSecurityTrustResourceUrl(this.url)
     
     });
+  }
+  showalert() {
+    Swal.fire({
+      title: 'LISTO',
+      text: 'Se ha registrado su comentario!',
+      imageUrl: 'https://cdn.discordapp.com/attachments/867213938096603138/920187718443622440/VOLUNTARITO_TENDENCIA.png',
+      imageWidth: 400,
+      imageHeight: 400,
+      imageAlt: 'Custom image',
+      showConfirmButton: false,
+      timer: 2700
+    })
   }
 }
