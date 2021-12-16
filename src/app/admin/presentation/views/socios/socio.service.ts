87ebd669177a6503cfe58db52b32d4f99771594e
b3nj4hb <benjalive12@gmail.com>
2021-12-16 03:00:43 -0500

    reporte socio
    4 servicios implementados y estilos aplicados

--- src/app/admin/presentation/views/socios/socio.service.ts
+++ src/app/admin/presentation/views/socios/socio.service.ts
@@ -12,9 +12,27 @@ export class SocioService {
     Headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
   private urlEndPoint: string = 'https://scabdi.herokuapp.com/api/persona'
+  private urlsocio: string = 'https://scabdi.herokuapp.com/api/socio'
+
   constructor(private http: HttpClient, private router: Router) { }
+  
+  // procedimiento josue
   getSocios(id:number): Observable<Persona[]> {
     return this.http.get<Persona[]>(`${this.urlEndPoint}/listarpersona/${id}`)
   }
   
+  // reporte socio
+  getSocio(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.urlsocio}/socio`)
+  }
+
+  getDatos(id:number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.urlsocio}/datos/${id}`)
+  }
+  getModulos(id:number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.urlsocio}/modulos/${id}`)
+  }
+  getPedidos(id:number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.urlsocio}/pedidos/${id}`)
+  }
 }
--- src/app/admin/presentation/views/socios/socios.component.ts
+++ src/app/admin/presentation/views/socios/socios.component.ts
@@ -9,16 +9,51 @@ import { SocioService } from './socio.service';
 })
 export class SociosComponent implements OnInit {
   persona: Persona[] = [];
+  socio: any[] = [];
+  datos: any[] = [];
+  modulos: any[] = [];
+  pedidos: any[] = [];
   constructor(private socioserv: SocioService) { }
 
   ngOnInit(): void {
-    this.listarsocios(1);
+    // this.listarsocios(5);
+    this.listarSocio();
+    // this.listarDatos(5);
+    // this.listarModulos(2);
+    // this.listarPedidos(5);
   }
 
-  listarsocios(id:any) {
+  // procedimiento josue
+  listarsocios(id: any) {
     this.socioserv.getSocios(id).subscribe(data => {
       this.persona = data;
       console.log(this.persona)
+    })
+  }
+
+  // reporte socio
+  listarSocio() {
+    this.socioserv.getSocio().subscribe(data => {
+      this.socio = data;
+      console.log(this.socio)
+    })
+  }
+  listarDatos(id: any) {
+    this.socioserv.getDatos(id).subscribe(data => {
+      this.datos = data;
+      console.log(this.datos)
+    })
+  }
+  listarModulos(id: any) {
+    this.socioserv.getModulos(id).subscribe(data => {
+      this.modulos = data;
+      console.log(this.modulos)
+    })
+  }
+  listarPedidos(id: any) {
+    this.socioserv.getPedidos(id).subscribe(data => {
+      this.pedidos = data;
+      console.log(this.pedidos)
     })
   }
 }
