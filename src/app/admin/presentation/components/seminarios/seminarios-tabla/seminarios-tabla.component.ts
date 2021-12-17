0f5ebf24e9c5df00056ad48c7e8ecd24a8a2dfb9
b3nj4hb <benjalive12@gmail.com>
2021-12-17 11:01:18 -0500

    orden

--- src/app/admin/presentation/components/seminarios/seminarios-tabla/seminarios-tabla.component.ts
+++ src/app/admin/presentation/components/seminarios/seminarios-tabla/seminarios-tabla.component.ts
@@ -1,16 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Seminario } from 'src/app/user/presentation/views/seminarios/seminarios';
+import { Digi2 } from '../../../views/seminarios/digi2';
+import { SeminariosService } from '../../../views/seminarios/seminarios.service';
 
 @Component({
   selector: 'adra-seminarios-tabla',
   templateUrl: './seminarios-tabla.component.html',
-  styles: [
-  ]
+  styleUrls: ['./st.css']
 })
 export class SeminariosTablaComponent implements OnInit {
 
-  constructor() { }
+  digi2: Digi2[] = []; //datos de la consulta digi, comentarios de socios
+  digi3: Seminario[] = []; //datos de seminario
+
+  constructor(private SeminariosService: SeminariosService) { }
 
   ngOnInit(): void {
+    // this.listardigi2(3);
+    this.listarseminarios();
   }
-
+  // funcion listar datos de la consulta
+  listardigi2(id:any) {
+    console.log(id.target.value)
+    // var c=Number(this.digi2[id].'valor id de la clase')
+    this.SeminariosService.getDigi2(id.target.value).subscribe(data => {
+      this.digi2 = data;
+      console.log(this.digi2)
+    })
+  }
+  // funcion listar seminarios
+  listarseminarios() {
+    this.SeminariosService.getSeminarios().subscribe(data => {
+      this.digi3 = data;
+      console.log(this.digi3)
+    })
+  }
 }
