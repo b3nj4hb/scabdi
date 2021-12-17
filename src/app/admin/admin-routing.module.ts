3bf424a661a7138ae3d574273308d2e7e6cd5c0a
b3nj4hb <benjalive12@gmail.com>
2021-12-16 22:44:06 -0500

    preparacion

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -6,6 +6,7 @@ import { PageModulosComponent } from './presentation/pages/page-modulos/page-mod
 import { PageSeminariosComponent } from './presentation/pages/page-seminarios/page-seminarios.component';
 import { PageSesionesComponent } from './presentation/pages/page-sesiones/page-sesiones.component';
 import { CrearseminarioComponent } from './presentation/views/crearseminario/crearseminario.component';
+import { ProgramacionComponent } from './presentation/views/programacion/programacion.component';
 import { SociosComponent } from './presentation/views/socios/socios.component';
 
 const routes: Routes = [
@@ -15,7 +16,8 @@ const routes: Routes = [
   {path: 'seminarios', component: PageSeminariosComponent},
   {path: 'crearseminario', component: CrearseminarioComponent},
   {path: 'socios', component: SociosComponent},
-  {path: 'modulosporbanco', component: ModulosTablaComponent}
+  {path: 'modulosporbanco', component: ModulosTablaComponent},
+  {path: 'programacion', component: ProgramacionComponent}
 ];
 
 @NgModule({
